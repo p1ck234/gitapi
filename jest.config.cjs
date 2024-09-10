d1ec60e20a9466baca21c3d2d66e7cfee7767c73
p1ck23 <dagobbo17@gmail.com>
2024-09-10 20:34:00 +0400

    config jest

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
+  moduleFileExtensions: ["js", "jsx"],
+};
